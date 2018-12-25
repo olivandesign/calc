ec891fd7fe7e4f0e585c35ef5e1712226ff446bc
Иван Олимпиев <olivan@MacBook-Pro-Ivan.local>
2018-12-25 18:53:09 +0300

    added basic logic

--- script.js
+++ script.js
@@ -1,40 +1,111 @@
-// function getControlPanel(layout) {
-
-// }
-// appNode = document.getElementsByClassName("app")[0];
-// controlsNode = document.getElementsByClassName("controls")[0];
-
 class Calculator { 
   constructor() {
-    this.currentOperation = this.clear();
-    this.operations = [];
+    this.currentOperation = { operandA: [], operandB: [], operator: null, result: null };
+    this.operationsHistory = [];
   }
 
-  clear() {
-    return {operandA: null, operandB: null, operationType: null,};
+  handleNumberClick(value) {
+    let { operandA, operandB, operator} = this.currentOperation;
+    if (!operator) {
+      operandA.push(value);
+    } else {
+      operandB.push(value);
+    }
+    console.log(this.currentOperation);
+    this.updateWindow(operandB.length === 0 ? operandA.join('') : operandB.join(''));
   }
 
+  handleOperationClick(value) {
+    let { operator } = this.currentOperation;
+    if (operator === "clear") {
+      this.newOperation();
+      this.updateWindow(0);
+      return;
+    }
+    if (!operator) {
+      this.currentOperation.operator = value;
+      console.log(this.currentOperation.operator);
+    } else this.executeOperation();
+  }
+  
+  updateWindow(value) {
+    resultNode.innerHTML = value;
+  }
+
+  newOperation() {
+    this.currentOperation = { operandA: [], operandB: [], operator: null, result: null };
+  }
+  
   divide(a, b) {
     return a / b;
   }
-
+  
   multiply(a, b) {
     return a * b;
   }
-
+  
   substract(a, b) {
     return a - b;
   }
-
+  
   add(a, b) {
     return a + b;
   }
 
-  count(operation) {
-    const { a, b, operationType} = operation;
-    return this[operationType](a, b);
+  executeOperation() {
+    const { operandA, operandB, operator } = this.currentOperation;
+    const a = +operandA.join('');
+    const b = +operandB.join('');
+    const operationType = operator
+    const result = this[operationType](a, b);
+    this.currentOperation.result = result
+    this.updateWindow(result);
+    this.newOperation();
+    this.currentOperation.operandA = result;
   }
 }
 
 let calc = new Calculator;
 
+resultNode = document.getElementsByClassName("result")[0];
+controlsNode = document.getElementsByClassName("controls")[0];
+
+controlsNode.onclick = function(e) {
+  const { target } = e;
+  let value = target.getAttribute("data-action");
+  
+  if (target.classList.contains("number")) {
+    calc.handleNumberClick(value)
+  } else if (target.classList.contains("operation")) {
+    calc.handleOperationClick(value);
+  } else return;
+}
+
+// handleNumberClick(value) {
+//   const { operandA, operandB, operationType } = this.currentOperation;
+//   const number = +value;
+//   if (!operationType) {
+//     operandA.push(number);
+//     this.updateResult(operandA.join(''));
+//   } else {
+//     if (operandA.length === 0) {
+//       this.clearOperationInfo();
+//       return;
+//     }
+//     operandB.push(number);
+//     this.updateResult(operandB.join(''));
+//   }
+// }
+
+// handleOperationClick(value) {
+//   if (value === "clear") {
+//     this.clearOperationInfo();
+//   }
+//   let { operationType } = this.currentOperation;
+//   if (!operationType) {
+//     operationType = value;
+//   } else {
+//     const operationInfo = this.getOperationInfo();
+//     this.count(operationInfo);
+//   }
+// }
