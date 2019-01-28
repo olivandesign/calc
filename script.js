07ffb4e23fa3127cf500ba1a0a326479b4bfd977
olivandesign <38014682+olivandesign@users.noreply.github.com>
2019-01-28 22:55:56 +0300

    Minor update

--- script.js
+++ script.js
@@ -1,159 +1,134 @@
-class Calculator { 
-  constructor() {
-    this.currentOperation = null;
+class Operation {
+  constructor(props) {
+    this.a = props.a ? props.a : null;
+    this.operator = props.operator ? props.operator : null;
+    this.b = null;
+    this.result = null;
+  }
+
+  divide() {
+    return this.a / this.b;
   }
   
-  //Operation Types
-  divide(a, b) {
-    return a / b;
+  multiply() {
+    return this.a * this.b;
   }
   
-  multiply(a, b) {
-    return a * b;
+  substract() {
+    return this.a - this.b;
   }
   
-  substract(a, b) {
-    return a - b;
-  }
-  
-  add(a, b) {
-    return a + b;
+  add() {
+    return this.a + this.b;
   }
   
   percent() {
-    return ;
+    return (this.a / 100) * this.b;
   }
 
-  // Click Handlers
-  handleControlClick(value) {
-    if (value === "clear") {
-      this.createNewOperation();
-    } else if (value === "result") {
-      this.convertInputToOperand();
-      this.executeOperation();
-    } else return;
-  }
-  
-  handleNumberClick(value) {
-    if (this.currentOperation.inputSessionIsClosed) {
-      this.createNewInputSession();
-    }
-    this.appendNumber(value)
-  }
-  
-  handleOperatorClick(value) {
-    this.closeInputSession();
-    this.addOperationType(value);
-    if (this.checkIfOperationIsPossible()) {
-      this.executeOperation();
-    }
-  }
-  
-  //Input / Output Services
-  appendNumber(number) {
-    this.currentOperation.input.push(number);
-    return;
+  execute() {
+    this.result = this[this.operator]();
+    return this.result;
   }
 
-  addOperationType(value) {
-    this.currentOperation.operationType = value;
-    return;
-  }
-
-  createNewOperatorSession() {
-    this.currentOperation.operatorSessionIsClosed = false;
-    this.currentOperation.operationType = null;
-  }
-
-  closeOperatorSession() {
-    this.currentOperation.operatorSessionIsClosed = true;
-    this.addOperationType();
-  }
-
-  createNewInputSession() {
-    this.currentOperation.inputSessionIsClosed = false;
-    this.currentOperation.input = [];
-  }
-  
-  closeInputSession() {
-    this.currentOperation.inputSessionIsClosed = true;
-    this.convertInputToOperand();
-  }
-  
-  convertInputToOperand () {
-    const { input, operandA } = this.currentOperation;
-    if (!operandA) {
-      this.currentOperation.operandA = parseInt(input.join(''), 10);
-    } else {
-      this.currentOperation.operandB = parseInt(input.join(''), 10);
-    }
-  }
-  
-  updateOutput() {
-    const { input, result } = this.currentOperation;
-    console.log(result);
-    const output = result ? result : input.join('');
-    resultNode.innerHTML = output;
-    return;
-  }
-  
-  //Operation Services
-  executeOperation() {
-    let { operandA, operandB, operationType} = this.currentOperation;
-    const result = this[operationType](operandA, operandB);
-    this.currentOperation.result = result;
-    this.setNextOperation();
-    return;
-  }
-  
-  setNextOperation() {
-    const result = this.currentOperation.result;
-    this.createNewOperation(); 
-    this.currentOperation.operandA = result;
-  }
-  
-  createNewOperation() {
-    this.currentOperation = { 
-      inputSessionIsClosed: true,
-      input: [0], 
-      operandA: null, 
-      operandB: null, 
-      operatorSessionIsClosed: true,
-      operationType: null, 
-      result: null 
-    };
-    return;
-    }
-
-  checkIfOperationIsPossible() {
-    const { operandA, operandB, operationType } = this.currentOperation;
-    let test = [operandA, operandB, operationType];
-    console.log('possibility of operation', test);
-    return (operandA && operandB && operationType) ? true : false;
-  }
-  
-  init() {
-    this.createNewOperation();
-    this.updateOutput();
+  checkIfExecutionIsPossible() {
+    return (this.a && this.b && this.operator) ? true : false;
   }
 }
 
+class InputSession {
+  constructor() {
+    this.value = [];
+    this.sessionIsClosed = false;
+  }
 
-let calc = new Calculator;
+  convertToDigit() {
+    return parseInt(this.value.join(''), 10);
+  }
+
+  closeSession() {
+    this.sessionIsClosed = true;
+    this.digit = this.convertToDigit();
+    return;
+  }
+}
+
+class Calculator { 
+  constructor() {
+    this.input = new InputSession;
+    this.currentOperation = null;
+    this.history = [];
+  }
+
+  handleDigitClick(value) {
+    if (this.input.sessionIsClosed) {
+      this.input = new InputSession;
+    }
+    this.input.value.push(value);
+    this.updateOutput(this.input.convertToDigit());
+  }
+
+  handleOperatorClick(value) {
+    if (!this.input.sessionIsClosed) {
+      const convertedInput = this.input.convertToDigit();
+      if (this.currentOperation.a) {
+        this.currentOperation.b = convertedInput;
+      } else {
+        this.currentOperation.a = convertedInput;
+      }
+      this.input.closeSession();
+    }
+
+    if (this.currentOperation.checkIfExecutionIsPossible()) {
+      const result = this.currentOperation.execute();
+      this.history.push(this.currentOperation);
+      this.createNewOperation({a: result});
+      this.updateOutput(result);
+      console.log(this.history);
+    }
+
+    this.currentOperation.operator = (value === "result") ? null : value;
+  }
+
+  handleServiceClick(value) {
+    if (value === "clear") {
+      this.init();
+    }
+    return;
+  }
+
+  updateOutput(value) {
+    resultNode.innerHTML = value;
+    return;
+  }
+
+  init() {
+    this.createNewOperation({});
+    this.updateOutput(0);
+  }
+
+  createNewOperation(operationProps) {
+    this.currentOperation = new Operation(operationProps);
+    return;
+  }
+}
+
 resultNode = document.getElementsByClassName("result")[0];
 controlsNode = document.getElementsByClassName("controls")[0];
+
+let calc = new Calculator;
 calc.init();
 controlsNode.onclick = function(e) {
   const { target } = e;
   let value = target.getAttribute("data-action");
   if (target.classList.contains("number")) {
-    calc.handleNumberClick(value);
+    calc.handleDigitClick(value);
   } else if (target.classList.contains("operator")) {
     calc.handleOperatorClick(value);
-  } else if (target.classList.contains("control")) {
-    calc.handleControlClick(value);
+  } else if (target.classList.contains("service")) {
+    calc.handleServiceClick(value);
   }
+  console.log(`input: ${calc.input.value}`);
   console.log(calc.currentOperation);
-  calc.updateOutput();
   return;
 }
